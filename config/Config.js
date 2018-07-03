ef7610723670d653e31d3699a2df85c0c6784dec
Andre Bomark <bomarkandre@gmail.com>
2018-07-03 02:08:41 +0200

    added: chapter 7 done - FB and Google auth - still a few errors

--- config/Config.js
+++ config/Config.js
@@ -1,4 +1,8 @@
 module.exports = { 
     DB: 'mongodb://localhost/movie_rating_app', 
-    SECRET: 'movieratingappsecretkey' 
+    SECRET: 'movieratingappsecretkey',
+    FACEBOOK_APP_ID: '189660465055442',  // client id (app id)
+    FACEBOOK_APP_SECRET: 'dd5ec9f6a383a80f920e53870b0369f6', // client app secret (app secret)
+    GOOGLE_APP_ID: '375280836385-lnc3varckanc8pnq2n5fm80iualivju0.apps.googleusercontent.com',   // client ID
+    GOOGLE_APP_SECRET: 'EaLx-bbR_QZkVCbTAB7Ufjn1'
 }
